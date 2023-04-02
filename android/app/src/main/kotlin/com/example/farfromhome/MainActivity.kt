485fbca2c5002e025230c8fb763fbd627d19f5c6
kavithabhandari <82359737+kavithabhandari@users.noreply.github.com>
2023-04-02 12:30:19 -0400

    declared image views, buttons and tv's

--- android/app/src/main/kotlin/com/example/farfromhome/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/farfromhome/MainActivity.kt
@@ -10,3 +10,14 @@ public class MainActivity extends AppCompatActivity {
     EditText etLocation, etNumRooms, etRent;
     Button btnAdd, btnView;
     ArrayList<House> houses = new ArrayList<>();
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etLocation = findViewById(R.id.etLocation);
+        etNumRooms = findViewById(R.id.etNumRooms);
+        etRent = findViewById(R.id.etRent);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnView = findViewById(R.id.btnView);
